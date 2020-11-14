ddbd78b75308cb505c7f34b3dd90ed9d1525c4c1
Ertan Turan <ertanturan@outlook.com>
2020-11-14 18:06:07 +0300

    coloring markers by their elevation

--- VolcanoData.py
+++ VolcanoData.py
@@ -2,7 +2,8 @@ import Coordinate
 
 
 class VolcanoData:
-    def __init__(self, coordinate, volcanoName):
+    def __init__(self, coordinate, volcanoName,elevation):
+        self.Elevation = elevation
         self.VolcanoName = volcanoName
         self.Coordinate = coordinate
 
--- WebmapMain.py
+++ WebmapMain.py
@@ -8,6 +8,14 @@ def ReadCSV(path):
     return pandas.read_csv(path)
 
 
+def ColorByElevation(elevation):
+    if elevation<1000:
+        return "green"
+    elif 1000<=elevation<3000:
+        return "orange"
+    else:
+        return "red"
+
 map = folium.Map(location=[39.913595, 32.854360], zoom_start=6, tiles="Stamen Terrain")
 
 volcanoesFeatureGroup = folium.FeatureGroup(name="VolcanoFeatureGroup")
@@ -16,15 +24,16 @@ volcanoesDataFrame = ReadCSV("Volcanoes.txt")
 
 print(len(volcanoesDataFrame))
 
-volcanoDatas = [VolcanoData.VolcanoData(cor.Coordinate(latLen, lonLen), nameLen)
-                for latLen, lonLen, nameLen in
+volcanoDatas = [VolcanoData.VolcanoData(cor.Coordinate(lat, lon), volcanoName,volcanoElevation)
+                for lat, lon, volcanoName,volcanoElevation in
                 zip(volcanoesDataFrame["LAT"],
-                    volcanoesDataFrame["LON"], volcanoesDataFrame["NAME"])]
+                    volcanoesDataFrame["LON"], volcanoesDataFrame["NAME"], volcanoesDataFrame["ELEV"])]
 
 for volcanoData in volcanoDatas:
     volcanoesFeatureGroup.add_child(
         folium.Marker(location=[volcanoData.Coordinate.Lat, volcanoData.Coordinate.Lon],
-                      popup=volcanoData.VolcanoName, icon=folium.Icon(color="green")))
+                      popup=volcanoData.VolcanoName,
+                      icon=folium.Icon(color=ColorByElevation(volcanoData.Elevation))))
 
 map.add_child(volcanoesFeatureGroup)
 
